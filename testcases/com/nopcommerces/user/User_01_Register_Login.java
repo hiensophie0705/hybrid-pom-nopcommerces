ab5fa2d13d7fc3404afac747360afbc21abb1f9c
Hien DT <dothihien070597@gmail.com>
2021-05-14 01:52:15 +0700

    Remove bin folder

--- testcases/com/nopcommerces/user/User_01_Register_Login.java
+++ testcases/com/nopcommerces/user/User_01_Register_Login.java
@@ -1,13 +1,20 @@
 package com.nopcommerces.user;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class User_01_Register_Login {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	
 	@BeforeClass
 	public void beforeClass() {
-		
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://demo.nopcommerce.com/");
 	}
 	@Test
 	public void TC_01_Register_Empty_Data() {
